221f050211e8da6e8a439bc392b3eb7ba7808da7
artiik2102 <artiik202010@gmail.com>
2026-03-30 16:51:09 +0300

    дз 2

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,16 @@
 from requests import get, post, delete
 
 # корректные
-print(get('http://localhost:5000/api/v2/users').json())
-print(post('http://localhost:5000/api/v2/users',
-           json={'surname': 'wgwgewgw',
-                 'name': "ddbdadуиуукwega",
-                 'age': 1352,
-                 'position': 'wrwwwrb',
-                 'speciality': 'egqggqeg',
-                 'address': 'qegqegqeqeg',
-                 'email': 'qegqegqegqewgq'}).json())
-print(delete('http://localhost:5000/api/v2/users/4').json())
+print(get('http://localhost:5000/api/v2/jobs').json())
+print(post('http://localhost:5000/api/v2/jobs',
+           json={'team_leader': 'wgwgewgw',
+                 'job': "ddbdadуиуукwega",
+                 'work_size': 1352,
+                 'collaborators': 'wrwwwrb',
+                 'start_date': 2026,
+                 'end_date': 2027,
+                 'is_finished': False}).json())
+print(delete('http://localhost:5000/api/v2/jobs/4').json())
 # не корректные
-print(post('http://localhost:5000/api/v2/users', json={'surname': 'wgwgewgw'}).json())
-print(delete('http://localhost:5000/api/v2/users/цутцштцйушпйошпош'))
+print(post('http://localhost:5000/api/v2/jobs', json={'job': 'wgwgewgw'}).json())
+print(delete('http://localhost:5000/api/v2/jobs/цутцштцйушпйошпош'))
